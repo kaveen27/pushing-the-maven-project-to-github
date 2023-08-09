ef83cf5dbddb8c8a727861a701f57b15764ab864
kaveen <kaveenmp@gmail.com>
2023-08-09 15:53:04 +0530

    the first push to github

---
+++ src/main/java/in/kavin/App.java
@@ -0,0 +1,13 @@
+package in.kavin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
